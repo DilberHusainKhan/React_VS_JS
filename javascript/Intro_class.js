4f267cf6a269a39d9697ea084f921ad291d9f301
dilberhusainkhan <dilberjmi@gmail.com>
2023-02-27 20:42:17 +0530

    JS CLass concept

--- javascript/Intro_class.js
+++ javascript/Intro_class.js
@@ -1,8 +1,17 @@
-class Person{
-    constructor(){
-        this.name = "Dilber"
-    }
-    print(){
+class Human{
+    gender = "Male";
 
+    printGender(){
+        console.log(this.gender);
     }
 }
+
+class Person extends Human{
+    name = "Dilber";
+    printName(){
+        console.log(this.name);
+    }
+}
+const person = new Person();
+person.printGender();
+person.printName();
